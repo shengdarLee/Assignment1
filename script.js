a31c3b572869a68e3250c90bd0e7fdfeb77aea1e
shengdarLee <shengdarlee@gmail.com>
2015-09-17 12:01:04 +0800

    Commit

---
+++ script.js
@@ -0,0 +1,202 @@
+//Assinment1 script.js
+// STEP 1
+/*
+var someMonth
+function theMonth
+currentMonth
+var summerMonth
+myLibraryAFunction
+*/
+
+// STEP 2
+/*
+3.1416 				// numeric literal 
+"Hello World!" 		// string literal 
+true 				// Boolean literal 
+null 				// literal null value 
+*/
+
+// STEP 3
+/*
+var aComplexExpression = 1 + 2 - 3 * (4 / 5);
+var aSecondComplexExpression = aComplexExpression % 6;
+*/
+
+// STEP 4
+/*
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var strZipCode;
+var nbrYourAge;
+var strReferralSource;
+var lgcMayWeContactYou;
+
+// STEP 5
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var strZipCode;
+var nbrYourAge;
+var strReferralSource;
+var lgcMayWeContactYou;
+strFirstName = "Shengdar";
+strLastName = "Lee";
+strAddress = "2412 Fairmount Ave";
+strCity = "San Diego";
+strState = "CA";
+strZipCode = "92105";
+nbrYourAge = 40;
+strReferralSource = "Continuing Education";
+lgcMayWeContactYou = true;
+
+var strFirstName = "Shengdar";
+var strLastName = "Lee";
+var strAddress = "2412 Fairmount Ave";
+var strCity = "San Diego";
+var strState = "CA";
+var strZipCode = "92105";
+var nbrYourAge = 40;
+var strReferralSource = "Continuing Education";
+var lgcMayWeContactYou = true;
+
+var strFirstName = "Shengdar", strLastName = "Lee", strAddress = "2412 Fairmount Ave", strCity = "San Diego", strState = "CA", strZipCode = "92105", nbrYourAge = 40, strReferralSource = "Continuing Education", lgcMayWeContactYou = true;
+*/
+
+// STEP 6
+/*
+var numberString = 5 + "five";
+console.log (numberString);
+var booleanString = true + "five";
+console.log (booleanString);
+var numberBoolean = 5 + true;
+console.log (numberBoolean);
+*/
+
+// STEP 7
+/*
+var theCountry= "China";
+console.log("My wife is from " + theCountry + " " + theProvince + ".");
+var theProvince = "Hunan";
+*/
+
+// STEP 8
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+// STEP 9
+/*
+var nullValue = null;
+console.log (nullValue);
+var undefinedValue;
+console.log (undefinedValue);
+*/
+
+// STEP 10
+/*
+console.log (typeof "Shengdar");
+console.log (typeof 3.1416);
+console.log (typeof true);
+console.log (typeof [1,2,3,4]);
+console.log (typeof Lee);
+*/
+
+// STEP 11
+//alert("Hello " +  "Shengdar Lee" + ", welcome to the JavaScript class!");
+    
+// STEP 12
+/*
+var name = "Shengdar Lee";
+alert("Hello " + name  + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+/*
+var name = "Shengdar Lee";
+var course = "JavaScript";
+alert("Hello " + name  + ", welcome to the " + course + "  class!");
+*/
+
+// STEP 14
+/*
+var name = "Shengdar Lee";
+var course = "JavaScript";
+alert("Hello " + name  + ",\nWelcome to the " + course + "  class!");
+*/
+
+// STEP 15
+/*
+var name = prompt("What is your name?");
+var course = "JavaScript";
+alert("Hello " + name  + ",\nWelcome to the " + course + "  class!");
+*/
+
+// STEP 16
+/*
+var name = prompt("What is your name?");
+var course = prompt("What course are you taking?");
+alert("Hello " + name  + ",\nWelcome to the " + course + "  class!");
+*/
+
+// STEP 17
+/*
+var x, y
+x = 10;
+y = 20;
+console.log(x+y);
+*/
+
+// STEP 18
+/*
+var x = 20;
+x += 20;
+console.log(x);
+*/
+
+// STEP 19
+/*
+var x = 20;
+x *= 5;
+console.log(x);
+*/
+
+// STEP 20
+/*
+var x = 20%3;
+x /= 1;
+console.log(x);
+*/
+
+// STEP 21
+/*
+var x = 5;
+var y = 10;
+console.log(x <= y && y%x == 0);
+*/
+
+// STEP 22
+/*
+var x = 5;
+var y = 10;
+console.log(x == y || y%x != 0);
+*/
+
+// STEP 23
+/*
+var widget = new Object();
+console.log(typeof widget);
+*/
+            
+// STEP 24
+var newString = new String("Shengdar");
+console.log(newString instanceof String);
+
+// STEP 25
+var newString = new String("Shengdar");
+console.log(newString instanceof Number);
